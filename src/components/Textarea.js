01cd1003d5735f0458271d8273d2fe6accb77bcf
Ekram70 <ekramullah70@gmail.com>
2022-08-05 23:55:06 +0600

    textarea component done

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -3,8 +3,10 @@ import styles from '../styles/Textarea.module.css';
 
 const Textarea = ({ text }) => {
   const [textArr, setTextArr] = useState([]);
+  const [outputArr, setOutputArr] = useState([]);
   const [isActive, setIsActive] = useState(true);
   const [isValid, setIsValid] = useState(true);
+  const [count, setCount] = useState(0);
   const editSpan = useRef(null);
   const tooltip = useRef(null);
 
@@ -13,8 +15,18 @@ const Textarea = ({ text }) => {
     editSpan.current.focus();
   }, [text]);
 
-  const generateSpans = () => {
-    return textArr.map((word, idx) => <span key={idx}>{word}</span>);
+  const generateSpans = (arr) => {
+    return arr.map((word, idx) => {
+      if (word.endsWith('xxxxx')) {
+        return (
+          <span key={idx} style={{ textDecoration: 'line-through' }}>
+            {word.replace('xxxxx', '')}
+          </span>
+        );
+      } else {
+        return <span key={idx}>{word}</span>;
+      }
+    });
   };
 
   const startFocus = () => {
@@ -24,10 +36,11 @@ const Textarea = ({ text }) => {
   const handleKeyPress = (e) => {
     setIsActive(false);
     if (textArr[0][0] === e.key) {
+      setIsValid(true);
       setTextArr((prevArr) => {
         return [prevArr[0].substring(1), ...prevArr.slice(1)];
       });
-    } else {
+    } else if (e.key !== ' ') {
       setIsValid(false);
     }
   };
@@ -36,15 +49,39 @@ const Textarea = ({ text }) => {
     if (e.key === 'Backspace') {
       let firstElem = editSpan.current.innerText.slice(-1) + textArr[0];
 
-      if (text[0] === editSpan.current.innerText.slice(0, -1) + textArr[0]) {
+      if (
+        text[count] ===
+        editSpan.current.innerText.slice(0, -1) + textArr[0]
+      ) {
         setIsValid(true);
       }
 
-      if (text[0] === editSpan.current.innerText + textArr[0]) {
+      if (text[count] === editSpan.current.innerText + textArr[0]) {
         setTextArr((prevArr) => {
           return [firstElem, ...prevArr.slice(1)];
         });
       }
+    }
+
+    if (e.key === ' ') {
+      setTextArr((prevArr) => {
+        return [...prevArr.slice(1)];
+      });
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === ' ') {
+      setCount((prevCount) => prevCount + 1);
+
+      setOutputArr((prevArr) => {
+        let spanText = editSpan.current.innerText.trimEnd();
+        if (text[count] !== spanText) {
+          spanText += 'xxxxx';
+        }
+        editSpan.current.innerText = '';
+        return [...prevArr, spanText];
+      });
     }
   };
 
@@ -58,15 +95,18 @@ const Textarea = ({ text }) => {
         Start Typing
       </span>
       <div className={styles.output}>
+        {generateSpans(outputArr)}
         <span
           ref={editSpan}
           className={isValid ? '' : styles.invalid}
+          spellCheck="false"
           contentEditable
           onKeyPress={handleKeyPress}
           onKeyDown={handleKeyDown}
+          onKeyUp={handleKeyUp}
         ></span>
       </div>
-      <div className={styles.sample}>{generateSpans()}</div>
+      <div className={styles.sample}>{generateSpans(textArr)}</div>
     </div>
   );
 };
